c75188c01b780f059355441480efca0ffad1cab0
Aman Alam <aalam@akamai.com>
2017-09-01 01:07:35 +0530

    wrote custom model code to exclude dates from returning

--- common/models/city.js
+++ common/models/city.js
@@ -2,4 +2,29 @@
 
 module.exports = function(City) {
 
+City.getAllCities = function(cb) {
+
+    City.find({fields: {name: true, countryname: true, lat: true, lng: true, thumburl: true, description: true} },
+    	function(err, result){
+
+    		//TODO : Handle errors
+    	cb(null, result);	
+    });
+
+  };
+
+
+  City.remoteMethod(
+    'getAllCities', {
+      http: {
+        path: '/getAllCities',
+        verb: 'get'
+      },
+      returns: {
+		arg: 'cities',
+		type: 'array'
+      }
+    }
+  );
+
 };
