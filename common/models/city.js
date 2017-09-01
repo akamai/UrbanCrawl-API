404180d1527aa7bd93ac9f26262d5ae704528c75
Aman Alam <aalam@akamai.com>
2017-09-01 23:47:17 +0530

    added endpoint to get a city by id, added model code to apply where and field filters to accomplish this

--- common/models/city.js
+++ common/models/city.js
@@ -4,7 +4,7 @@ module.exports = function(City) {
 
 City.getAllCities = function(cb) {
 
-    City.find({fields: {name: true, countryname: true, lat: true, lng: true, thumburl: true, description: true} },
+    City.find({fields: {id:true, name: true, countryname: true, lat: true, lng: true, thumburl: true, description: true} },
     	function(err, result){
 
     		//TODO : Handle errors
@@ -22,7 +22,41 @@ City.getAllCities = function(cb) {
       },
       returns: {
 		arg: 'cities',
+		description: 'Returns a JSON array of all the available cities, mainly to use in City List screen',
 		type: 'array'
+      }
+    }
+  );
+
+
+  City.getCityDetails = function(idToFind, next) {
+
+    City.find({where: {id: idToFind}, fields: {createdate: false, lastUpdated: false} },
+    	function(err, result){
+
+    		//TODO : Handle errors
+    	next(null, result);	
+    });
+
+  };
+
+  City.remoteMethod(
+    'getCityDetails', {
+      http: {
+        path: '/getCityDetails',
+        verb: 'get'
+      },
+      accepts: {
+      	arg: 'id', 
+      	type: 'number', 
+      	http: {
+      		source: 'query'
+      	}
+      },
+      returns: {
+		arg: 'cityDetails',
+		description: 'Returns a JSON object containing details and places of a city whose ID is supplied',
+		type: 'Object'
       }
     }
   );
