0bfed90613df1d4b492c2a9143ad037956f5d573
Aman Alam <aalam@akamai.com>
2017-09-07 17:58:10 +0530

    added search API

--- common/models/city.js
+++ common/models/city.js
@@ -19,6 +19,8 @@ City.disableRemoteMethod('__updateById__places', false );
 City.disableRemoteMethod('__findById__places', false );
 City.disableRemoteMethod('__get__places', false );
 
+
+//--------- Get All Cities ------------
 City.getAllCities = function(cb) {
 
 	City.find({fields: {id:true, name: true, countryname: true, lat: true, lng: true, thumburl: true, description: true} },
@@ -48,6 +50,8 @@ City.getAllCities = function(cb) {
 	);
 
 
+
+//---------- Get City Details ------------------
 City.getCityDetails = function(idToFind, cb) {
 
 	var placesOfCity;
@@ -99,6 +103,63 @@ City.getCityDetails = function(idToFind, cb) {
 		arg: 'cityDetails',
 		description: 'Returns a JSON object containing details and places of a city whose ID is supplied',
 		type: 'Object'
+      }
+    }
+  );
+
+
+//------------- Search Cities -------------
+
+City.search = function(keyword, cb) {
+
+	var placesOfCity;
+
+  	if(keyword === undefined){
+  		var error = new Error("No keyword was supplied. You must supply a search keyword");
+  		error.status = 404;
+  		cb(error, null);
+  	}else{
+
+    	City.find({
+    		where: {name: {like: "%"+keyword+"%"}}, 
+    		fields: {id:true, name: true, countryname: true, lat: true, lng: true, thumburl: true, description: true}
+    	},
+    	function(err, result){
+
+			if(!err){
+				if(result.length > 0){
+					cb(null, result);
+				}else{
+					var error = new Error("Didn't find anything with this keyword");
+			  		error.status = 404;
+			  		cb(error, null);
+				}
+			}else{
+				var error = new Error("Something went wrong and we couldn't fulfil this request. Write to us if this persists");
+		  		error.status = 500;
+		  		cb(error, null);
+			}
+    	});
+  	}
+  };
+
+  City.remoteMethod(
+    'search', {
+      http: {
+        path: '/search',
+        verb: 'get'
+      },
+      accepts: {
+      	arg: 'q', 
+      	type: 'string', 
+      	http: {
+      		source: 'query'
+      	}
+      },
+      returns: {
+		arg: 'cities',
+		description: 'Returns a JSON array of all the available cities that match the supplied keyword, mainly to use in City List screen',
+		type: 'array'
       }
     }
   );
