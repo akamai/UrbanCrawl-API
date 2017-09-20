d763eb28dcb2545a360611ebec840e28246a402b
Aman Alam <aalam@akamai.com>
2017-09-20 23:23:56 +0530

    added getPlaceDetails API

--- common/models/place.js
+++ common/models/place.js
@@ -51,4 +51,53 @@ Place.getAllPlacesOfCity = function(idToFind, cb) {
     }
   );
 
+  Place.getPlaceDetails = function(idToFind, cb) {
+
+  if(idToFind === undefined){
+      var error = new Error("No id was supplied. You must supply a cidy id");
+      error.status = 404;
+      cb(error, null);
+    }else{
+      Place.find({where: {id: idToFind}, fields: {createdate: false, lastupdated: false, cityid: false} },
+      function(err, result){
+
+        if(!err){
+        if(result.length > 0){
+          cb(null, result[0]);
+        }else{
+          var error = new Error("Didn't find anything with this id");
+            error.status = 404;
+            cb(error, null);
+        }
+      }else{
+        var error = new Error("Something went wrong and we couldn't fulfil this request. Write to us if this persists");
+          error.status = 500;
+          cb(error, null);
+      }
+    });
+    }
+  };
+
+
+  Place.remoteMethod(
+    'getPlaceDetails', {
+      http: {
+        path: '/getPlaceDetails',
+        verb: 'get'
+      },
+      accepts: {
+        arg: 'id', 
+        type: 'number', 
+        http: {
+          source: 'query'
+        }
+      },
+      returns: {
+    arg: 'placeDetails',
+    description: 'Returns a JSON array of all the available places, belonging to the city whose id is supplied',
+    type: 'Object'
+      }
+    }
+  );
+
 };
