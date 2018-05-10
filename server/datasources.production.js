731211c6e30737769c522c338b72850c36a01417
Aman Alam <aalam@akamai.com>
2018-05-10 15:44:06 +0530

    readded production config with env variables

---
+++ server/datasources.production.js
@@ -0,0 +1,10 @@
+module.exports = {
+  db: {
+    connector: 'mysql',
+    hostname: process.env.CLEARDB_DATABASE_URL,
+    port: process.env.CLEARDB_PORT,
+    user: process.env.CLEARDB_USERNAME,
+    password: process.env.CLEARDB_PASSWORD,
+    database: process.env.CLEARDB_NAME,
+  }
+};
